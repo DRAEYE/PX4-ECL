11347bc6613830d71e92f07b324a3fbb58ebbbb0
Daniel Agar <daniel@agar.ca>
2020-10-29 15:20:08 -0400

    EKF: add mag bias reset helper and update IMU bias reset to match

--- EKF/estimator_interface.h
+++ EKF/estimator_interface.h
@@ -198,13 +198,6 @@ public:
 	// set vehicle landed status data
 	void set_in_air_status(bool in_air) {_control_status.flags.in_air = in_air;}
 
-	/*
-	Reset all IMU bias states and covariances to initial alignment values.
-	Use when the IMU sensor has changed.
-	Returns true if reset performed, false if rejected due to less than 10 seconds lapsed since last reset.
-	*/
-	virtual bool reset_imu_bias() = 0;
-
 	// return true if the attitude is usable
 	bool attitude_valid() { return ISFINITE(_output_new.quat_nominal(0)) && _control_status.flags.tilt_align; }
 
@@ -337,9 +330,6 @@ public:
 			return false;
 		}
 	}
-
-	virtual Vector3f getAccelBias() const = 0;
-	virtual Vector3f getGyroBias() const = 0;
 
 	// get EKF mode status
 	void get_control_mode(uint32_t *val)
